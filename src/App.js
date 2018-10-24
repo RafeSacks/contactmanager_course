f6791ab4a0cce18fd4301e18ec9c422c6a9295b1
Rafe Sacks <rafesacks@gmail.com>
2018-10-24 08:53:55 -0600

    Switch projhect back to using AddContact form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Header from "./components/layout/Header";
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
-import AddContactByRef from "./components/contacts/AddContactByRef";
 import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,7 +14,7 @@ class App extends Component {
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
-            <AddContactByRef />
+            <AddContact />
             <Contacts />
           </div>
         </div>
